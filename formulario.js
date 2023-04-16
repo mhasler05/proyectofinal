75ea2df305a37ca86ce7d46ada5892a98714a541
WalterG13 <130024823+WalterG13@users.noreply.github.com>
2023-04-15 23:23:45 -0300

    Update formulario.js
    que no se ingresen números en nombre y apellido.
    generar pdf

--- formulario.js
+++ formulario.js
@@ -22,14 +22,14 @@ $(document).ready(function() {
            messages: {
                 nombre: {
                     required: "Este campo es requerido",
-                    minlength: "Debe tener al menos 3 letras"
+                    minlength: "Debe tener al menos 3 letras",
                 },
                 apellido: {
                     required: "Este campo es requerido",
     
                 },
                 email: {
-                    email: "El email no tiene el formato requerido"
+                    email: "El email no tiene el formato requerido",
                 },
                 telefono: {
                     requiered: "Este campo es requerido",
@@ -44,7 +44,20 @@ $(document).ready(function() {
       this.value = this.value.replace(/[^0-9]/g, '');
     });
   });
-  
+$(document).ready(function() {
+    $("#nombre").on("input", function() {
+      this.value = this.value.replace(/[^a-zA-Z]/g, '');
+    });
+  });
+
+  $(document).ready(function() {
+    $("#apellido").on("input", function() {
+      this.value = this.value.replace(/[^a-zA-Z]/g, '');
+    });
+  });
+    
+
+
 var campo1=$("#nombre");
 var campo2=$("#apellido");
 var campo3=$("#email");
@@ -64,3 +77,46 @@ var campo4=$("#telefono");
             )
 
     });
+document.addEventListener("DOMContentLoaded", () => {
+        const $boton = document.querySelector("#botonPDF");
+        $boton.addEventListener("click", () => {
+            const $elementoParaConvertir = document.body; 
+            html2pdf()
+                .set({
+                    margin: 1,
+                    filename: 'documento.pdf',
+                    image: {
+                        type: 'jpeg',
+                        quality: 0.98,
+                    },
+                    html2canvas: {
+                        scale: 3, 
+                        letterRendering: true,
+                    },
+                    jsPDF: {
+                        unit: "in",
+                        format: "a3",
+                        orientation: "portrait",
+                    }
+                })
+                .from($elementoParaConvertir)
+                .save()
+                .catch(err => console.log(err));
+        });
+    });
+
+    var campo1=$("#nombre");
+    var campo2=$("#apellido");
+    var campo3=$("#email");
+    var miBoton=("#botonGuardar")  
+
+    
+        function verificarCampos() {
+            if (campo1.val() != "" && campo2.val() != "" && campo3.val() != "") {
+              miBoton.prop("disabled", false);
+            } else {
+              miBoton.prop("disabled", true);
+            }
+          };
+    
+          verificarCampos();
